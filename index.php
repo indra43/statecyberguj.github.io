9793145dfa68723f9d05ce835278a206fbbad106
indra43 <indrabbit@gmail.com>
2024-10-25 12:10:22 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+include('header-front.php');
+?>
+<main>
+
+
+      <section class="section  d-flex  align-items-center justify-content-center py-4" style="background: url('assets/img/home.jpg') center/cover no-repeat;height: 80vh;">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-md-8 d-flex flex-column align-items-center justify-content-center" id="homesec">
+              <h2>Raise Unfreeze Account Request</h2>
+              <p>If you are not a fraudster and mule account holder, and you believe that your account has been wrongly frozen, you can raise your request to unfreeze your account.</p>
+              <a href="raise-request.php" class="btn btn-primary w-50">Raise Request</a>
+            </div>
+          </div>
+        </div>
+    </section>
+
+   <!--  <section class="right" id='homesec'>
+        <h2>Check Request Status</h2>
+        <p>Check the status of your unfreeze account request using the provided request ID.</p>
+        <a href="check-status.php" class="btn btn-danger w-50">Check Status</a>
+    </section> -->
+<section class="section contact">
+      <div class="row">
+      
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d117337.83380228395!2d72.57140380121447!3d23.213692035134468!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395c2bf084f4c7e1%3A0x515e6d471e0c55e!2sState%20Cyber%20Crime%20Cell%2C%20CID%20Crime!5e0!3m2!1sen!2sin!4v1704190427374!5m2!1sen!2sin"  height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+
+    </section>
+
+
+  </main>
+<?php
+include('footer-front.php');
+?>
